e2e0fb109f90f5bd972aa81c468cbdf68b52ff88
AngelSanchezAW <angelsanchez314@gmail.com>
2016-04-19 02:07:08 -0500

    Condición: color de imagen.

--- JS/script.js
+++ JS/script.js
@@ -1,32 +1,17 @@
 function generar(){
-
-    var tipos = new Array("abstract","animals","business","city","food","nightlife","fashion","people","nature","sports","technics","transport");
-    
+    var tipos = new Array("abstract","animals","business","city","food","nightlife","fashion","people","nature","sports","technics","transport"); 
 	tipo = document.propiedades.tipoImg.value
 	alto = document.propiedades.alto.value
 	ancho = document.propiedades.ancho.value
-	imgColor = "Imagen a color";
-
+	color = document.propiedades.cb.value   
 	var tipoImage = tipos[tipo];
-	var urlimg = "http://lorempixel.com/"+alto+"/"+ancho+"/"+tipoImage+"/";
-
-	document.getElementById("imgContenedor").innerHTML='<img onclick="textoIMG()" title="Ver imagen" src="'+urlimg+'" data-large-src="'+urlimg+'">';
-	$("img").h5lightbox();
-	document.getElementById("titulo").innerHTML='Tamaño de imagen: '+alto+' x '+ancho+' | '+imgColor+'';
-}
-
-function generarg(){
-
-    var tipos = new Array("abstract","animals","business","city","food","nightlife","fashion","people","nature","sports","technics","transport");
-
-	tipo = document.propiedades.tipoImg.value
-	alto = document.propiedades.alto.value
-	ancho = document.propiedades.ancho.value
-	imgColor = "Imagen a escala de grises";
-
-	var tipoImage = tipos[tipo];
-
-	var urlimg = "http://lorempixel.com/g/"+alto+"/"+ancho+"/"+tipoImage+"/";
+    if (color==12) {
+    	var urlimg = "http://lorempixel.com/"+alto+"/"+ancho+"/"+tipoImage+"/";
+    	imgColor = "Imagen a color";
+    } else {
+    	var urlimg = "http://lorempixel.com/g/"+alto+"/"+ancho+"/"+tipoImage+"/";
+    	imgColor = "Imagen a escala de grises";
+    }	
 	document.getElementById("imgContenedor").innerHTML='<img onclick="textoIMG()" title="Ver imagen" src="'+urlimg+'" data-large-src="'+urlimg+'">';
 	$("img").h5lightbox();
 	document.getElementById("titulo").innerHTML='Tamaño de imagen: '+alto+' x '+ancho+' | '+imgColor+'';
